1ddfbe46089a177e7a4fe4ad0bf72f8ec0df519e
The_Horned_Rat <alexandre.fernandez@enti.cat>
2024-12-11 12:57:32 +0100

    Creamos la herramienta para incluir ratillas en el clan
    Fer un cin per agafar noms de ratilles

--- PlagaSkaven/PlagaSkaven/Source.cpp
+++ PlagaSkaven/PlagaSkaven/Source.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 void main() {
-	std::cout << "HELLO NO-FURS" << std::endl;
+	std::cout << "HELLO NO-FURS Give me your-your name" << std::endl;
+
+	std::string username;
+	std::cin >> username;
+	std::cout << "Saludos " <<  username  << " , Bienvenido al Clan" << std::endl;
 
 }
